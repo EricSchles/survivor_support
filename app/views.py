0d4bdfb86b017a49ef65e2e98a4a0e57ca26cc52
Eric Schles <ericschles@gmail.com>
2019-05-10 16:57:38 -0400

    adding bootstrap

--- app/views.py
+++ app/views.py
@@ -1,8 +1,44 @@
 from app import app
 from app.models import Table
 import json
+from flask import Blueprint, render_template, flash, redirect, url_for
+from flask_bootstrap import __version__ as FLASK_BOOTSTRAP_VERSION
+from flask_nav.elements import Navbar, View, Subgroup, Link, Text, Separator
+from markupsafe import escape
+
+from .forms import SignupForm
+from .nav import nav
+
+frontend = Blueprint('frontend', __name__)
+
+# We're adding a navbar as well through flask-navbar. In our example, the
+# navbar has an usual amount of Link-Elements, more commonly you will have a
+# lot more View instances.
+nav.register_element('frontend_top', Navbar(
+    View('Flask-Bootstrap', '.index'),
+    View('Home', '.index'),
+    View('Forms Example', '.example_form'),
+    View('Debug-Info', 'debug.debug_root'),
+    Subgroup(
+        'Docs',
+        Link('Flask-Bootstrap', 'http://pythonhosted.org/Flask-Bootstrap'),
+        Link('Flask-AppConfig', 'https://github.com/mbr/flask-appconfig'),
+        Link('Flask-Debug', 'https://github.com/mbr/flask-debug'),
+        Separator(),
+        Text('Bootstrap'),
+        Link('Getting started', 'http://getbootstrap.com/getting-started/'),
+        Link('CSS', 'http://getbootstrap.com/css/'),
+        Link('Components', 'http://getbootstrap.com/components/'),
+        Link('Javascript', 'http://getbootstrap.com/javascript/'),
+        Link('Customize', 'http://getbootstrap.com/customize/'), ),
+    Text('Using Flask-Bootstrap {}'.format(FLASK_BOOTSTRAP_VERSION)), ))
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     results = [elem.row for elem in Table.query.all()]
     return json.dumps(results)
+
+@app.route("/things", methods=["GET", "POST"])
+def testing():
+    return render_template("index.html")
