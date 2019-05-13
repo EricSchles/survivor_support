1f21a64041a4bb332081f8a7a59bee7669ff3f7b
Eric Schles <ericschles@gmail.com>
2019-05-13 14:38:54 -0400

    Adding urls modle

--- app/models.py
+++ app/models.py
@@ -3,9 +3,21 @@ from app import db
 class Table(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     row = db.Column(db.String(400))
-
+    
     def __init__(self, row):
         self.row = row
 
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+class URLs(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(1000))
+    keyword = db.Column(db.String(400))
+    
+    def __init__(self, url, keyword):
+        self.url = url
+        self.keyword = keyword
+        
     def __repr__(self):
         return '<id {}>'.format(self.id)
--- app/views.py
+++ app/views.py
@@ -1,13 +1,20 @@
 from app import app
-from app.models import Table
+from app.models import URLs
 import json
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 
 @app.route("/query_backend", methods=["GET", "POST"])
 def index():
     results = [elem.row for elem in Table.query.all()]
     return json.dumps(results)
 
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    keyword = request.form
+    results = [elem.url
+               for elem in URLs.query.filter_by(keyword=keyword).all()]
+    return render_template("results.html", results=results)
+               
 @app.route("/", methods=["GET", "POST"])
 def testing():
     return render_template("index.html")
