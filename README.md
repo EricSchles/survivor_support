d62544699fcb68effa9391708930f2bd8f2dd064
Eric Schles <ericschles@gmail.com>
2019-05-13 11:09:55 -0400

    removing bootstrap from the backend

---
+++ README.md
@@ -0,0 +1,17 @@
+Verification:
+
+phone call
+type of service
+reviews of service
+professionals on staff
+
+
+Create an individualized profile:
+
+	-> 
+
+Multiple choice quiz for annotation
+
+Polaris
+crisis text line - https://www.crisistextline.org/referrals
+
--- app/__init__.py
+++ app/__init__.py
@@ -4,20 +4,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from .commands import REPL
 import os
-from flask_bootstrap import Bootstrap
-from .nav import nav
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-app.config["BOOTSTRAP_SERVE_LOCAL"] = True
 #app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://eric_s:1234@localhost/testing"
 db = SQLAlchemy(app)
-bootstrap = Bootstrap(app)
 migrate = Migrate(app,db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 manager.add_command("shell", REPL())
-nav.init_app(app)
 
 from app import views, models
